ae1fb28faf696d369a89555cc3c61c9e59c86bae
sharmila003 <sharmilakanthuri055@gmail.com>
2023-12-20 21:36:10 +0530

    initial commit

--- app.js
+++ app.js
@@ -37,8 +37,12 @@ app.get("/subscribers/names",async(req,res)=>{
 //api to get subscribers by id || METHOD GET
 app.get("/subscribers/:id",async(req,res)=>{
   try{
-    let subscribers = await Subscriber.findById(req.params.id);
-    res.status(200).send(subscribers);
+    let subscriber = await Subscriber.findById(req.params.id)
+    if(subscriber) {
+      res.status(200).json(subscriber)
+      }else {
+      res.status(404).json({message: "Subscribers not found"})
+      }
   }catch(error){
     res.status(400).send({message : error.message});
   }
